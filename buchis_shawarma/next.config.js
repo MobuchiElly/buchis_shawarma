0287e1beaac551710a493306e5109a67a4130706
MobuchiElly <ugwueleazer@gmail.com>
2023-12-27 08:51:49 +0100

    fixing endpoint

--- buchis_shawarma/next.config.js
+++ buchis_shawarma/next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const endpointUrl = process.env.ENDPOINT_URL || 'http://localhost:3000'
+
 const nextConfig = {
   // reactStrictMode: true,
   images:{
     domains:["res.cloudinary.com"]
+  },
+  env: {
+    ENDPOINT_URL: endpointUrl,
   }
 }
 
