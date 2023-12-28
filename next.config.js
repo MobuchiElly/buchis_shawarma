ed56940feb2e33c24f4b5b5b511279745c314a3a
MobuchiElly <ugwueleazer@gmail.com>
2023-12-28 19:04:34 +0100

    updates

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-const endpointUrl = process.env.ENDPOINT_URL || 'http://localhost:3000'
+// const endpointUrl = process.env.ENDPOINT_URL || 'http://localhost:3000'
 
 const nextConfig = {
   // reactStrictMode: true,
   images:{
     domains:["res.cloudinary.com"]
   },
-  env: {
-    ENDPOINT_URL: endpointUrl,
-  }
+  // env: {
+  //   ENDPOINT_URL: endpointUrl,
+  // }
 }
 
 module.exports = nextConfig
