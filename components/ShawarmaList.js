1120c49a4d81eaf1ee032b32c6cfbb36599dbdda
MobuchiElly <ugwueleazer@gmail.com>
2023-12-30 17:16:15 +0100

    update to text

--- components/ShawarmaList.js
+++ components/ShawarmaList.js
@@ -5,7 +5,7 @@ const PizzaList = ({ shawarmaList }) => {
   return (
     <div className={styles.container}>
       <h1 className={`${styles.title} text-3xl font-bold`}>THE BEST SHAWARMA IN TOWN</h1>
-          <p className={styles.desc}>BuchisShawarma invites you on a culinary journey like no other, where savory flavors and aromatic spices come together to create an unforgettable shawarma experience. Our commitment to quality and passion for authentic taste make us the go-to destination for shawarma enthusiasts</p>
+          <p className={styles.desc}>Buchis Shawarma invites you on a culinary journey like no other, where savory flavors and aromatic spices come together to create an unforgettable shawarma experience. Our commitment to quality and passion for authentic taste make us the go-to destination for shawarma enthusiasts</p>
       <div className={styles.wrapper}>
         {shawarmaList.map((val) => (
                 <ShawarmaCard shawarma={val} key={val._id}/>
