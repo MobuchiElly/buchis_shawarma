8fe836e463810536f07b5fd91025ea573aa7f0e9
MobuchiElly <ugwueleazer@gmail.com>
2024-07-23 10:51:02 +0100

    ui update to featured component

--- components/Featured.js
+++ components/Featured.js
@@ -16,7 +16,7 @@ const Featured = ({ shawarmaList }) => {
     }
     
   return ( 
-    <div className='bg-main-bg-600 overflow-hidden' style={{height:'calc(100vh - 130px)'}}>
+    <div className='bg-main-bg-600 overflow-hidden h-[79vh]'>
         <div className="h-full relative">
             <div className='absolute top-0 bottom-0 m-auto cursor-pointer' style={{left: '0', height:'10%', width:'10%', zIndex:'2'}} onClick={() => handleClick('l')}>
                 <Image src="/img/arrowl.png" alt="scroll left" fill={true} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" style={{objectFit:"contain"}}/>
