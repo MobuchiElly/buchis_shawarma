8005f1669d40beb9cee805f8278b51290608a012
MobuchiElly <ugwueleazer@gmail.com>
2024-07-23 23:43:31 +0100

    order summary page ui update

--- components/Pagination.js
+++ components/Pagination.js
@@ -3,15 +3,15 @@ import { FaArrowLeft, FaArrowRight, FaArrowUp, FaArrowDown } from 'react-icons/f
 const Pagination = ({activePage, setactivePage, handlePagination}) => {
   return (
     <div className="min-h-10 py-6 px-24 flex flex-col">
-        <div className="border border-b-black h-1 w-full mb-1"></div>
+        <div className="border border-b-black w-full mb-4"></div>
         <div className="flex justify-center p-2 space-x-2">
-            <div className={`shadow-md py-2 px-5 border border-black text-black text-lg cursor-pointer ${activePage === 1 ? "font-bold" : 'font-normal'}`} onClick={() => setactivePage(1)}>
+            <div className={`shadow-md py-1 px-4 border border-black text-black text-lg cursor-pointer ${activePage === 1 ? "font-bold" : 'font-normal'}`} onClick={() => setactivePage(1)}>
                 1
             </div>
-            <div className={`shadow-md py-2 px-5 border border-black text-black text-lg cursor-pointer ${activePage === 2 ? "font-bold" : 'font-normal'}`} onClick={() => setactivePage(2)}>
+            <div className={`shadow-md py-1 px-4 border border-black text-black text-lg cursor-pointer ${activePage === 2 ? "font-bold" : 'font-normal'}`} onClick={() => setactivePage(2)}>
                 2
             </div>
-            <div className="shadow-md py-3 px-3 border border-black cursor-pointer" onClick={() => setactivePage(2)}>
+            <div className="shadow-md py-1 px-2 border border-black cursor-pointer" onClick={() => setactivePage(2)}>
             <FaArrowRight size={24} color="black" className="p-1"/>
             </div>
         </div>
